0f465f69e0b0905f9685741df72797ce38bcbead
dna737 <adnanhere27@gmail.com>
2023-11-12 02:15:43 -0600

    add workspaces and users support

--- backend/backend.js
+++ backend/backend.js
@@ -3,9 +3,9 @@ const { MongoClient } = require("mongodb");
 const { collection } = require("./Schemas/Workspace");
 
 async function retrieveData(queryName, collectionName) {
-
     const mongoURI = process.env.MONGO_URI || "-1";
-    paths = null;
+    let paths = null;
+    let workspaces = null;
 
     // Create a MongoDB client
     const client = new MongoClient(mongoURI, {});
@@ -18,27 +18,35 @@ async function retrieveData(queryName, collectionName) {
 
         const collection = db.collection(collectionName);
 
-        const query = { workspaceName: queryName };
-
+        let query = null;
+        if (collectionName === "workspaces")
+            query = { workspaceName: queryName };
+        else if (collectionName === "users") query = { username: queryName };
         const results = await collection.find(query).toArray();
 
         if (results.length == 0) {
-            console.log("File not found. Returning null.")
-            return paths
+            console.log("File not found. Returning null.");
+            return paths;
         }
 
-        for (const result of results) {
-            paths = result.paths;
-            console.log(paths);
+        if (collectionName === "workspaces") {
+            for (const result of results) {
+                paths = result.paths;
+                console.log(paths);
+            }
+        } else {
+            for (const result of results) {
+                workspaces = result.workspaces;
+            }
         }
-
     } finally {
         await client.close();
         //console.log("Connection closed");
     }
-    return paths;
+    return paths || workspaces;
 }
 
 //retrieveData("jf83lsi890--workspace2", "workspaces");
-retrieveData("3qliuvbwlriey-esl118", "workspaces");
+// retrieveData("fssfasf", "workspaces");
+retrieveData("hallu", "users").then((result) => console.log(result));
 module.exports = retrieveData;
